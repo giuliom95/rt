8c8785d78e728819860441f8ffaf6e1215603ff8
giuliom95 <giuliomartella1995@gmail.com>
2017-08-20 11:41:12 +0200

    First Point implementation

---
+++ src/math/Point.h
@@ -0,0 +1,33 @@
+#ifndef POINT_H
+#define POINT_H
+
+#include "Vector.h"
+
+class Point
+{
+public:
+	double x, y, z;
+
+	Point() : x{0}, y{0}, z{0} {}
+	Point(double x, double y, double z) : x{x}, y{y}, z{z} {}
+
+	inline			double& operator[](unsigned);
+	inline const	double& operator[](unsigned) const;
+
+	Vector	operator- (const Point& p) {return {x-p.x, y-p.y, z-p.z};}
+
+	Point	operator+ (const Vector& v) {return {x+v.x, y+v.y, z+v.z};}
+	Point	operator- (const Vector& v) {return (*this)+(-v);}
+};
+
+const double& Point::operator[](unsigned idx) const
+{
+	switch(idx) {case 0: return x; case 1: return y; case 2: return z;}
+}
+
+double& Point::operator[](unsigned idx)
+{
+	switch(idx) {case 0: return x; case 1: return y; case 2: return z;}
+}
+
+#endif //POINT_H
