3e98a22e92a71465baaf559627d1792f90c91c65
giuliom95 <giuliomartella1995@gmail.com>
2017-08-23 19:22:14 +0200

    Added simple obj importer

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -4,4 +4,27 @@
 
 Scene::Scene(std::string path) : vertices {}
 {
+	std::vector<Point> tmp;
+	std::ifstream ifs(path);
+
+	while(!ifs.eof())
+	{
+		char first;
+		ifs >> first;
+		if(first == 'v')
+		{
+			Point v;
+			ifs >> v.x >> v.y >> v.z;
+			tmp.push_back(v);
+		}
+
+		if(first == 'f')
+		{
+			int i1, i2, i3;
+			ifs >> i1 >> i2 >> i3;
+			vertices.push_back(tmp[i1-1]);
+			vertices.push_back(tmp[i2-1]);
+			vertices.push_back(tmp[i3-1]);
+		}
+	}
 }
