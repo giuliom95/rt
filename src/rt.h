c36077e43334c523a3b43e68e6950b4dc29f6849
giuliom95 <giuliomartella1995@gmail.com>
2017-08-24 17:34:37 +0200

    Added general header (whoops I didn't commited it when I created it)

---
+++ src/rt.h
@@ -0,0 +1,13 @@
+#ifndef RT_H
+#define RT_H
+
+#include <utility>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+#define PI 3.14159265358979323846
+
+#endif // RT_H
